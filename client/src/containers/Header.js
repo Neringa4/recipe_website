f0b715d669491d85dfa33aba2720607256ecc757
Neringa4 <neringasiugzdinyte@gmail.com>
2023-10-16 14:15:09 +0100

    Fetch categories

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,11 +1,26 @@
 import TitleBar from "../components/TitleBar";
 import CategoriesBar from "../components/CategoriesBar";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.edamam.com/doc/open-api/recipe-search-v2.json")
+        .then(res => res.json())
+        .then(data => setCategories([7, 8, 9, 10, 11].map(index => {
+            const categoryName = data.paths["/api/recipes/v2"].get.parameters[index].name
+            const categoryValues = data.paths["/api/recipes/v2"].get.parameters[index].items.enum
+            return(
+                {[categoryName]: categoryValues}
+            )
+        })))
+    }, [])
+
     return(
         <>
             <TitleBar/>
-            <CategoriesBar/>
+            <CategoriesBar categories={categories}/>
         </>
     )
 }
