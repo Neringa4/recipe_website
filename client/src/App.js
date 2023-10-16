5bc5ff108d266f6aa17238d1edd08001d0c06092
Neringa4 <neringasiugzdinyte@gmail.com>
2023-10-16 11:43:23 +0100

    Add browser routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import Header from "./containers/Header";
 import MainPage from "./containers/MainPage";
 import RecipePage from "./containers/RecipePage";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-    </>
+      <Routes>
+        <Route path="/" element={<MainPage/>}></Route>
+        <Route path="/recipes/:label" element={<RecipePage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
