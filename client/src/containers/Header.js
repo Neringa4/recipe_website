14adf619ab5d030d17a51f843dd5264044279939
Neringa4 <neringasiugzdinyte@gmail.com>
2023-10-16 13:37:38 +0100

    Add header components

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,5 +1,13 @@
-const Header = () => {
+import TitleBar from "../components/TitleBar";
+import CategoriesBar from "../components/CategoriesBar";
 
+const Header = () => {
+    return(
+        <>
+            <TitleBar/>
+            <CategoriesBar/>
+        </>
+    )
 }
 
 export default Header;
