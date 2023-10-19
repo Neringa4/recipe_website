faeffac676ff52be0c128e2f0174fff91afe6aad
Neringa4 <neringasiugzdinyte@gmail.com>
2023-10-19 10:24:53 +0100

    Populate categories.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,41 @@ import Header from "./containers/Header";
 import MainPage from "./containers/MainPage";
 import RecipePage from "./containers/RecipePage";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import categories from './categories.js';
 
 function App() {
+  // const [categories, setCategories] = useState([
+  //   {Meals: []},
+  //   {Dishes: []},
+  //   {Cuisines: []},
+  //   {Diet: []},
+  //   {Health: []}
+  // ]);
+  
+  // useEffect(() => {
+  //   fetch("https://api.edamam.com/doc/open-api/recipe-search-v2.json")
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     const newData = [10, 11, 9, 7, 8].map((i) => {
+  //       const types = data.paths["/api/recipes/v2"].get.parameters[i].items.enum
+  //       const title = data.paths["/api/recipes/v2"].get.parameters[i].name
+  //       return({[title]: types})
+  //     })
+  //     setCategories(categories.map((cat, index) => {
+  //       const catKey = Object.keys(categories[index])[0]
+  //       const urlKey = Object.keys(newData[index])[0]
+  //       cat[catKey] = newData[index][urlKey].map(type => {
+  //         return({displayTitle: type, url: `&${urlKey}=${type}`})
+  //       })
+  //       return(cat)
+  //     }))
+  //   })
+  // }, [])
+
   return (
     <BrowserRouter>
-      <Header/>
+      <Header categories={categories}/>
       <Routes>
         <Route path="/" element={<MainPage/>}></Route>
         <Route path="/recipes/:label" element={<RecipePage/>}></Route>
---
+++ client/src/categories.js
@@ -0,0 +1,105 @@
+const categories = [
+    {
+        Meals: [
+            {displayTitle: 'Breakfast', url: '&mealType=Breakfast'},
+            {displayTitle: 'Dinner', url: '&mealType=Dinner'},
+            {displayTitle: 'Lunch', url: '&mealType=Lunch'},
+            {displayTitle: 'Snack', url: '&mealType=Snack'},
+            {displayTitle: 'Teatime', url: '&mealType=Teatime'}
+        ]
+    },
+    {
+        Dishes: [
+            {displayTitle: 'Biscuits and cookies', url: '&dishType=Biscuits and cookies'},
+            {displayTitle: 'Bread', url: '&dishType=Bread'},
+            {displayTitle: 'Cereals', url: '&dishType=Cereals'},
+            {displayTitle: 'Condiments and sauces', url: '&dishType=Condiments and sauces'},
+            {displayTitle: 'Desserts', url: '&dishType=Desserts'},
+            {displayTitle: 'Drinks', url: '&dishType=Drinks'},
+            {displayTitle: 'Main course', url: '&dishType=Main course'},
+            {displayTitle: 'Pancake', url: '&dishType=Pancake'},
+            {displayTitle: 'Preps', url: '&dishType=Preps'},
+            {displayTitle: 'Preserve', url: '&dishType=Preserve'},
+            {displayTitle: 'Salad', url: '&dishType=Salad'},
+            {displayTitle: 'Sandwiches', url: '&dishType=Sandwiches'},
+            {displayTitle: 'Side dish', url: '&dishType=Side dish'},
+            {displayTitle: 'Soup', url: '&dishType=Soup'},
+            {displayTitle: 'Starter', url: '&dishType=Starter'},
+            {displayTitle: 'Sweets', url: '&dishType=Sweets'}
+        ]
+    },
+    {
+        Cuisine: [
+            {displayTitle: 'American', url: '&cuisineType=American'},
+            {displayTitle: 'Asian', url: '&cuisineType=Asian'},
+            {displayTitle: 'British', url: '&cuisineType=British'},
+            {displayTitle: 'Caribbean', url: '&cuisineType=Caribbean'},
+            {displayTitle: 'Central Europe', url: '&cuisineType=Central Europe'},
+            {displayTitle: 'Chinese', url: '&cuisineType=Chinese'},
+            {displayTitle: 'Eastern Europe', url: '&cuisineType=Eastern Europe'},
+            {displayTitle: 'French', url: '&cuisineType=French'},
+            {displayTitle: 'Indian', url: '&cuisineType=Indian'},
+            {displayTitle: 'Italian', url: '&cuisineType=Italian'},
+            {displayTitle: 'Japanese', url: '&cuisineType=Japanese'},
+            {displayTitle: 'Kosher', url: '&cuisineType=Kosher'},
+            {displayTitle: 'Mediterranean', url: '&cuisineType=Mediterranean'},
+            {displayTitle: 'Mexican', url: '&cuisineType=Mexican'},
+            {displayTitle: 'Middle Eastern', url: '&cuisineType=Middle Eastern'},
+            {displayTitle: 'Nordic', url: '&cuisineType=Nordic'},
+            {displayTitle: 'South American', url: '&cuisineType=South American'},
+            {displayTitle: 'South East Asian', url: '&cuisineType=South East Asian'}
+        ]
+    },
+    {
+        Diet: [
+            {displayTitle: 'Vegan', url: '&health=vegan'},
+            {displayTitle: 'Vegetarian', url: '&health=vegetarian'},
+            {displayTitle: 'Pescatarian', url: '&health=pescatarian'},
+            {displayTitle: 'Balanced', url: '&diet=balanced'},
+            {displayTitle: 'Gluten-free', url: '&health=gluten-free'},
+            {displayTitle: 'Low-fat', url: '&diet=low-fat'}
+        ]
+    },
+    {
+        Other: [ 
+            {displayTitle: 'Alcohol-cocktail', url: '&health=alcohol-cocktail'},
+            {displayTitle: 'Alcohol-free', url: '&health=alcohol-free'},
+            {displayTitle: 'Celery-free', url: '&health=celery-free'},
+            {displayTitle: 'Crustacean-free', url: '&health=crustacean-free'},
+            {displayTitle: 'Dairy-free', url: '&health=dairy-free'},
+            {displayTitle: 'DASH', url: '&health=DASH'},
+            {displayTitle: 'Egg-free', url: '&health=egg-free'},
+            {displayTitle: 'Fish-free', url: '&health=fish-free'},
+            {displayTitle: 'Fodmap-free', url: '&health=fodmap-free'},
+            {displayTitle: 'High-fibre', url: '&diet=high-fiber'},
+            {displayTitle: 'High-protein', url: '&diet=high-protein'},
+            {displayTitle: 'Immuno-supportive', url: '&health=immuno-supportive'},
+            {displayTitle: 'Keto-friendly', url: '&health=keto-friendly'},
+            {displayTitle: 'Kidney-friendly', url: '&health=kidney-friendly'},
+            {displayTitle: 'Kosher', url: '&health=kosher'},
+            {displayTitle: 'Low-carb', url: '&diet=low-carb'},
+            {displayTitle: 'Low-fat-abs', url: '&health=low-fat-abs'},
+            {displayTitle: 'Low-potassium', url: '&health=low-potassium'},
+            {displayTitle: 'Low-sodium', url: '&diet=low-sodium'},
+            {displayTitle: 'Low-sugar', url: '&health=low-sugar'},
+            {displayTitle: 'Lupine-free', url: '&health=lupine-free'},
+            {displayTitle: 'Mediterranean', url: '&health=Mediterranean'},
+            {displayTitle: 'Mollusk-free', url: '&health=mollusk-free'},
+            {displayTitle: 'Mustard-free', url: '&health=mustard-free'},
+            {displayTitle: 'No-oil-added', url: '&health=no-oil-added'},
+            {displayTitle: 'Paleo', url: '&health=paleo'},
+            {displayTitle: 'Peanut-free', url: '&health=peanut-free'},
+            {displayTitle: 'Pork-free', url: '&health=pork-free'},
+            {displayTitle: 'Red-meat-free', url: '&health=red-meat-free'},
+            {displayTitle: 'Sesame-free', url: '&health=sesame-free'},
+            {displayTitle: 'Shellfish-free', url: '&health=shellfish-free'},
+            {displayTitle: 'Soy-free', url: '&health=soy-free'},
+            {displayTitle: 'Sugar-conscious', url: '&health=sugar-conscious'},
+            {displayTitle: 'Sulfite-free', url: '&health=sulfite-free'},
+            {displayTitle: 'Tree-nut-free', url: '&health=tree-nut-free'},
+            {displayTitle: 'Wheat-free', url: '&health=wheat-free'}
+        ]
+    }
+]
+
+export default categories;
