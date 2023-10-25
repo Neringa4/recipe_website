fba2c8bfb5eb1b4c74168803db310bce4c9e16de
Neringa4 <neringasiugzdinyte@gmail.com>
2023-10-25 16:41:53 +0100

    Change title

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const TitleBar = () => {
     return(
         <Link to="/" id="header-title-link">
-            <h1 id="header-title">Recipes</h1>
+            <h1 id="header-title">RecipeDatabase</h1>
         </Link>
     )
 }
