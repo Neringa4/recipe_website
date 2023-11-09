c0ce9a31440d03c8dbdef02116cd6988a10f3a87
Neringa4 <neringasiugzdinyte@gmail.com>
2023-11-09 11:25:12 +0000

    Add advanced search link to header

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -4,12 +4,16 @@ import SearchBar from './SearchBar';
 
 const TitleBar = ({fetchRecipes}) => {
     return(
-        <>
+        <div className="header-container">
             <Link to="/" id="header-title-link">
                 <h1 id="header-title">RecipeDatabase</h1>
             </Link>
-            <SearchBar fetchRecipes={fetchRecipes}/>
-        </>
+            <div className="search-container">
+                <SearchBar fetchRecipes={fetchRecipes}/>
+                <span className="divider"></span>
+                <Link to="/advanced-search" id="advanced-search-link">Advanced Search</Link>
+            </div>
+        </div>
     )
 }
 
